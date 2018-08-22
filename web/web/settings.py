6b1e9a1c154dd89c19123b3e2efbaa7be835b95d
felexkemboi <felokemboi10@gmail.com>
2018-08-22 16:13:51 +0300

    ffff

--- web/web/settings.py
+++ web/web/settings.py
@@ -134,4 +134,4 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static')
