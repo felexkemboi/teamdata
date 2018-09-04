7f6a436dd2a30f1296d50aa785d7a18d8532809c
felexkemboi <felokemboi10@gmail.com>
2018-09-04 14:08:34 +0300

    yes

--- team/team/wsgi.py
+++ team/team/wsgi.py
@@ -7,17 +7,30 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
+"""
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "team.settings")
 
-application = get_wsgi_application()
+application = get_wsgi_application()"""
 
-
+"""
 
 from whitenoise.django import DjangoWhiteNoise
 
 application = DjangoWhiteNoise(application)
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+"""
+import os
+"""
+from whitenoise import WhiteNoise"""
+from django.core.wsgi import get_wsgi_application
+"""
+from whitenoise.django import DjangoWhiteNoise"""
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "team.settings")
+application = get_wsgi_application()
+"""application = WhiteNoise(application)"""
