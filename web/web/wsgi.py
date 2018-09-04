000931e7b0db005bbe2b286940dd049a9f2f324c
felexkemboi <felokemboi10@gmail.com>
2018-09-04 12:44:33 +0300

    eze

--- web/web/wsgi.py
+++ web/web/wsgi.py
@@ -6,22 +6,21 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
-
-#import os
-#from whitenoise.django import DjangoWhiteNoise
-#from django.core.wsgi import get_wsgi_application
-
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
-
-#application = get_wsgi_application()
-#application = DjangoWhiteNoise(application)
-
-
 """
+import os
+from whitenoise.django import DjangoWhiteNoise
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
+
+
 
 from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
-
 """
 import os
 """
@@ -31,6 +30,6 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise"""
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webp.settings")
 application = get_wsgi_application()
 """application = WhiteNoise(application)"""
